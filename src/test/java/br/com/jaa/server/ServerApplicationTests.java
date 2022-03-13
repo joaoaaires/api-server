901838de2e00e6e546594a172b0763e7dfb93ae8
João Aires <joao.aires@sovis.com.br>
2022-03-12 22:14:35 -0300

    primeiro commit

---
+++ src/test/java/br/com/jaa/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jaa.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
