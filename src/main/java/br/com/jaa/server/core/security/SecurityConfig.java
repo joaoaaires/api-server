d9fcd8f83a7e2c650c02c422bc99c8310d0cf109
Joao Aires <jantonioayres@gmail.com>
2022-12-07 20:10:16 -0300

    ajustado autenticacao como na versao anterior

--- src/main/java/br/com/jaa/server/core/security/SecurityConfig.java
+++ src/main/java/br/com/jaa/server/core/security/SecurityConfig.java
@@ -3,9 +3,15 @@ package br.com.jaa.server.core.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -26,8 +32,10 @@ public class SecurityConfig {
                                 .requestMatchers(HttpMethod.POST, "/usuario/signup", "/usuario/signin").permitAll()
                                 .requestMatchers(HttpMethod.GET, "/").permitAll()
                                 .anyRequest().authenticated()
+                                .and()
+                                .addFilter(securityFilter())
                 )
-
+                .httpBasic(Customizer.withDefaults())
                 .build();
 //        http
 //                .cors().and().csrf().disable()
@@ -60,6 +68,18 @@ public class SecurityConfig {
 //    public SecurityToken securityToken() {
 //        return new SecurityToken();
 //    }
+
+    @Bean
+    public AuthenticationManager authenticationManager() {
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+        return new ProviderManager(daoAuthenticationProvider);
+    }
+
+    @Bean
+    public SecurityFilter securityFilter() {
+        return new SecurityFilter(authenticationManager());
+    }
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
