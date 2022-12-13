fe3da42767d857cdf06990cca3bda6408e3cf599
Joao Aires <jantonioayres@gmail.com>
2022-12-12 22:06:34 -0300

    ajustado filter para basic auth

--- src/main/java/br/com/jaa/server/core/security/SecurityFilter.java
+++ src/main/java/br/com/jaa/server/core/security/SecurityFilter.java
@@ -1,14 +1,24 @@
 package br.com.jaa.server.core.security;
 
 import br.com.jaa.server.core.exceptio.ApiServerException;
+import br.com.jaa.server.features.usuario.entities.Usuario;
 import br.com.jaa.server.features.usuario.repositories.UsuarioCrudRepository;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
 import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
 
 public class SecurityFilter extends BasicAuthenticationFilter {
 
@@ -40,30 +50,6 @@ public class SecurityFilter extends BasicAuthenticationFilter {
             String id = values[0];
             String tokenBase64 = values[1];
 
-            /*
-            List<GrantedAuthority> authorities = Arrays.asList(new SimpleGrantedAuthority("ROLE_USER"));
-            UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
-                    "admin",
-                    null,
-                    authorities
-            );
-            SecurityContextHolder.getContext().setAuthentication(authentication);
-            */
-        } catch (ApiServerException ex) {
-            ex.printStackTrace();
-        } finally {
-            chain.doFilter(request, response);
-        }
-    }
-
-    /*
-    @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
-
-            /*
-
-
-
             Optional<Usuario> optionalUsuario = usuarioCrudRepository.findById(Long.valueOf(id));
             if (optionalUsuario.isEmpty()) {
                 throw new ApiServerException("usuario não encontrado");
@@ -71,22 +57,28 @@ public class SecurityFilter extends BasicAuthenticationFilter {
 
             Usuario usuario = optionalUsuario.get();
 
-            Claims claims = Jwts.parser()
-                    .setSigningKey(usuario.getPassword().getBytes())
-                    .parseClaimsJws(tokenBase64)
-                    .getBody();
+//            Claims claims = Jwts.parser()
+//                    .setSigningKey(usuario.getPassword().getBytes())
+//                    .parseClaimsJws(tokenBase64)
+//                    .getBody();
 
-            if (!id.equals(claims.getId())) {
-                throw new ApiServerException("token não pertence a esse usuario");
-            }
+//            if (!id.equals(claims.getId())) {
+//                throw new ApiServerException("token não pertence a esse usuario");
+//            }
+//
+//            securityHelper.gerarNovoToken(claims, usuario, response);
 
-            securityHelper.gerarNovoToken(claims, usuario, httpResponse);
-
-            usuarioLogged.set(usuario);
-
-
-
-
+            List<GrantedAuthority> authorities = Arrays.asList(new SimpleGrantedAuthority("ROLE_USER"));
+            UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
+                    usuario,
+                    null,
+                    authorities
+            );
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        } catch (ApiServerException ex) {
+            ex.printStackTrace();
+        } finally {
+            chain.doFilter(request, response);
+        }
     }
-    */
 }
